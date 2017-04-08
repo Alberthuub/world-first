c9b5f5fcb19d509bea504b26b8574c0f8617c498
Albert <Albert@ALBERTEIBACAC34>
2017-04-08 10:36:30 +0200

    Add a comment

---
+++ the last one/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_last_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.writeline("hello world");
+            console.readkey();
+        }
+    }
+}
